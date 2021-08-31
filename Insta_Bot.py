45bc429e7529af51daa07d9042884af585926bb9
Arne <nbdyknowme@protonmail.com>
2021-08-31 10:12:09 +0200

    added instabot

--- Insta_Bot.py
+++ Insta_Bot.py
@@ -1,48 +1,91 @@
+import selenium.webdriver.common.touch_actions
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import os
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
-#define webdriver
+#define webdriver and emulate IPhone X
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
+mobile_emulation = { "deviceName": "iPhone X" }
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+driver = webdriver.Chrome(PATH, options=chrome_options)
 driver.get("https://www.instagram.com/")
-cookie = {'domain': 'www.instagram.com', 'httpOnly': False, 'name': 'foo', 'path': '/', 'secure': True, 'value': 'bar'}
-driver.add_cookie(cookie)
-print(driver.get_cookies())
 
-#wait 10s or till cookies loaded and accept
-#element = WebDriverWait(driver, 10).until(
-#    EC.presence_of_element_located((By.TAG_NAME, "button")))
-#element.click()
+class Instagram:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.counter  = 0
 
-#type username account, wait max 10s until searchbar loaded
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.NAME, "username")))
-element.send_keys("rn.brndl")
-#hit enter
-element.send_keys(Keys.RETURN)
+    def login(self):
+        #accept cookies and press login button
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/button[1]")))
+        element.click()
+        sleep(1)
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/section/main/article/div/div/div/div[3]/button[1]")))
+        element.click()
+        #type in username and password
+        element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/main/article/div/div/div/form/div[1]/div[3]/div/label/input")))
+        element.send_keys(self.username)
+        element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/section/main/article/div/div/div/form/div[1]/div[4]/div/label/input")))
+        element.send_keys(self.password)
+        #click login button
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/section/main/article/div/div/div/form/div[1]/div[6]/button")))
+        element.click()
+        #press dont safe information button
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/section/main/div/div/div/button")))
+        element.click()
+        #press dont add instagram to favoruite sides
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div/div/div/div[3]/button[2]")))
+        element.click()
+        #scroll down, so the activate notification button appears
+        driver.execute_script("window.scrollBy(0,2000)", "")
+        #press false on activate notifiation
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div/div/div/div[3]/button[2]")))
+        element.click()
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/section/div[2]/div/div[1]/button/span")))
+        element.click()
 
-#type password
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.NAME, "password")))
-element.send_keys("Arne2005")
-#hit enter
-element.send_keys(Keys.RETURN)
+    def goToStart(self):
+        driver.get("https://www.instagram.com/")
 
-#press the login button, press enter
-element.send_keys(Keys.RETURN)
+    def likeByHashtag(self,hashtags,amount):
+        #open instagram site with the tag
+        for hashtag in hashtags:
+            driver.implicitly_wait(60)
+            driver.get('https://www.instagram.com/explore/tags/' + hashtag)
+            for i in range(0,amount+1):
+                # find all pictures and put into list
+                elements = driver.find_elements_by_class_name("_9AhH0")
+                #take one element out of list
+                element = elements[i]
+                #open it
+                element.click()
+                element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/section/main/div/div/article/div[3]/section[1]/span[1]/button/div/span")))
+                element.click()
+                sleep(6)
+                #go back
+                driver.back()
 
-#click false on safe ur login information
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//button[contains(@class,'sqdOP yWX7d')]")))
-print(element)
+    def upload(self, image, text):
+        driver.get("https://www.instagram.com/")
+        element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/section/nav[2]/div/div/div[2]/div/div/div[3]"))).click()
+        #find input field
+        upload_btn = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
+        time.sleep(2)
+        #send image
+        upload_btn.send_keys("C:\\Users\\brend\PycharmProjects\ClimateCoinBot\\PP2.jpg") #full path of the file which is to be uploaded
 
-sleep(30)
+acc = Instagram("climatecoinbot", "Arne2005")
+acc.login()
+acc.upload()
+#rn.likeByHashtag(["hello"],10)
+sleep(120)
 
 #quit
 driver.quit()
